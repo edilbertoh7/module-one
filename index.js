343404dd5b79b56dbf546125f0a15c1580b48f31
edilbertoh7 <46673520+edilbertoh7@users.noreply.github.com>
2024-01-31 00:09:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
+import database from './img/databases.png'
+import computer_lang from './img/computer-lang.png'
+import laptopanimado from './img/laptopanimado.png'
+import mouse from './img/mouse.png'
+import development from './img/development.png'
+import claqueta from './img/claqueta.png'
 
-const quotes = [
-    "En esta isla vivió, durante cuatro largos años, Robinson Crusoe, cuya historia no solamente fascinó y emocionó al mundo entero sino que puso en el mapa del mundo a esta isla en la cual viven ochocientas chilenas y chilenos.",
-    "Y durante nuestro Gobierno, vamos a entregar cinco nuevos ‘tiatros’ regionales en Iquique, La Serena, ‘Rancuagua’, Concepción y Punta Arenas.",
-    "Si usted maneja, no conduce.",
-    "Nunca han mirado las estrellas, la galactea o el fondo del alma? Una cosa es mirar e intentar descubrir y otra cosa ver que es lo sensorial",
-    "Marepoto",
-    "Tusunami",
-    "Es la misma bandera con que hemos ‘cubrido’ tantas veces los féretros de nuestros ‘mártis’",
-    "Ha llegado el fin de año, tiempo en que nos preguntamos, qué hicimos bien, qué hicimos mal, qué ‘podimos’ haber hecho mejor"
-  ];
-  
-  /**
-   * Gets a random Piñera Quote
-   * @returns {string}
-   */
-  function randomQuote() {
-    return quotes[Math.floor(Math.random() * quotes.length)];
-  }
-  
-  
-  module.exports = {
-    randomQuote
-  };
+export const img_service = [
+    {
+        id: 1,
+        title:"Permítenos ayudarte a que tu negocio sea visible en internet",
+        img: mouse, 
+        comment: "Cada vez es mas importante tener presencia en internet ya que esto permite que tu empresa o negocio llegue a mas personas, para ello teofrecemos un portafolio de servicios a tu medida orientados a interactuar de manera efectiva con tus clientes."
+    },
+    {
+        id: 2,
+        title:"Desarrollo de software, edición de audio y video animación digital ¡todo en un solo lugar!",
+        img: databases, 
+        comment: "Nos ajustamos a tus necesidades de crecimiento, por ello te ofrecemos servicios multimedia en general y desarrollo de software a tu medida ya sea aplicaciones web o de escritorio"
+    },
+    {
+        id: 3,
+        title: "Edición de video y animación digital con herramientas de excelente calidad",
+        img: claqueta,
+        comment: "Las estrategias de video marketing son fundamentales para impactar a tus clientes y lograr un fuerte crecimiento a tu negocio, para ello realizamos proyectos de animación digital y contenido multimedia en general desarrollando piezas audiovisuales de excelente calidad"
+    },
+    {
+        id: 4,
+        title: "Aplicaciones web y de escritorio escalables",
+        img: computer_lang, 
+        comment: "Todas las aplicaciones que desarrollamos tanto  web como de escritorio son escalables así que no te preocupes si tu empresa tiene crecimiento tu software esta listo para crecer contigo"
+    },
+    {
+        id: 5,
+        title: "Aplicaciones web e intranet a tu medida",
+        img: laptopanimado,
+        comment: "No es solo crear una pagina web sino crear toda una infraestructura donde tus clientes puedan interactuar con tu empresa *esto no solo garantizara tu presencia en internet, sino que además como valor agregado el aumento sustancial de tus ventas*"
+    },
+    {
+        id: 6,
+        title: "Desarrollamos software segun tus necesidades o las de tu empresa",
+        img: development,
+        comment: "¿Porque ajustarse a programas o software de terceros cuando puedes hacer que el software se adapte a tus necesidades? No lo pienses mas desarrollamos software a la medida de las necesidades de tu empresa, esto te permitirá personalizar tus procesos y gestionar mejor el tiempo"
+    }
 
-// importo axios
+]
+
+
+
+
+
